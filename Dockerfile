4c91db5019595aa9ba7a65ae42e344a7bda04464
Kenneth Dave <dave.bahana@gmail.com>
2024-11-23 22:36:18 +0700

    Dockerfile adjustment

--- Dockerfile
+++ Dockerfile
@@ -11,10 +11,16 @@ COPY requirements.txt .
 RUN apt-get update && apt-get install -y --no-install-recommends \
     libgl1-mesa-glx \
     libglib2.0-0 \
+    curl \
     && rm -rf /var/lib/apt/lists/*
 
 RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
+
+
+RUN curl -o /capstone-project-442502-e205627d1062.json "https://drive.google.com/uc?export=download&id=17XFCjdg7g6hMB28QC_DeiresCxvhulso" \
+    && curl -o /firebase-credential.json "https://drive.google.com/uc?export=download&id=1eIKnsBTWTSoeZQVTwAJJpr7iWJaE4DuS"
+
 
 COPY . .
 
--- app/common/config.py
+++ app/common/config.py
@@ -4,12 +4,12 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "OCR + GenAI App"
     debug: bool = False
-    ocr_model_path: str
-    project_id: str
-    location: str
-    credentials: str
-    database: str
-    firebase_credentials: str
+    ocr_model_path: str = "./models"
+    project_id: str = "capstone-project-442502"
+    location: str = "us-central1"
+    credentials: str = "capstone-project-442502-e205627d1062.json"
+    database: str = "bangkit-db"
+    firebase_credentials: str = "firebase-credential.json"
 
     class Config:
         env_file = ".env"
