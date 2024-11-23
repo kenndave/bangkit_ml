b56788af49ff7cd2ed007072489b5be85ed15fd3
Kenneth Dave <dave.bahana@gmail.com>
2024-11-23 17:26:29 +0700

    Edit main.py port

--- app/main.py
+++ app/main.py
@@ -29,4 +29,4 @@ app.include_router(embedding_router_v1.router, prefix="/api/v1", tags=["Embeddin
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
